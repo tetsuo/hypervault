6b3727866340af1f81972b8fe9f312bf205bacec
tetsuo <ogunduz@gmail.com>
2014-11-18 09:51:29 +0200

    Release 0.1.0

--- test/config.py
+++ test/config.py
@@ -2,9 +2,9 @@
 conf = dict(
   db_conns = [
     dict(shards='1-9', host='', port='5432',
-      user='x', password='x', database='hvtest'),
+      user='onur', password='z', database='hvtest'),
     dict(shards='9-17', host='192.168.2.24', port='5432',
-      user='x', password='x', database='hvtest')
+      user='onur', password='z', database='hvtest')
   ],
   db_pool_max = 10,
   db_pool_block_timeout = 2
